6cf37c624b8b7cac6950a67de7c51d89d433bace
Cory Caswell <Corneleus1@gmail.com>
2017-12-30 15:16:31 -0500

    added entity model from db and created a controler to show the list of live action movies from db

---
+++ Media Tracker App/Media Tracker App/Api/LiveActionMoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Media_Tracker_App.Models;
+
+namespace Media_Tracker_App.Api
+{
+    public class LiveActionMoviesController : ApiController
+    {//path is /api/liveactionmovies/getmovies
+        DBEntities _ctx = null; 
+
+        public LiveActionMoviesController()
+        {
+            _ctx = new DBEntities();
+        }
+        public List<Live_Action_Movies> GetMovies()
+        {
+            List<Live_Action_Movies> movies = null;
+            try
+            {
+                movies = _ctx.Live_Action_Movies.ToList();
+            }
+            catch
+            {
+                movies = null;
+            }
+            return movies;
+        }
+    }
+}
---
+++ Media Tracker App/Media Tracker App/Controllers/LiveActionMovies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Media_Tracker_App.Controllers
+{
+    public class LiveActionMovies : Controller
+    {
+        // GET: Lamoives
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
